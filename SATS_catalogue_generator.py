acdce676ff2c9120e14450361a97422e7e949949
ADablanc <axel.alain.dablanc@gmail.com>
2022-07-19 15:34:00 +0200

    "SATS_catalogue_generator.py" browsing correction

--- SATS_catalogue_generator.py
+++ SATS_catalogue_generator.py
@@ -6,36 +6,34 @@ main_path = "./DATA/Papers"
 
 SATS_dict = {}
 
-for folders in os.listdir(main_path):
-    sub_folders = os.path.join(main_path, folders)
-    for sub_sub_folders in os.listdir(sub_folders):
-        files_path = os.path.join(sub_folders, sub_sub_folders)
-        for files in os.listdir(files_path):
-            if files.endswith("bibheliotech_V1.txt"):
-                liste = []
-                with open(os.path.join(files_path,files)) as file:
-                    Lines = file.readlines()
-                    for line in Lines:
-                        if not re.search("^#",line): # skip comments lines
-                            sat = line.split(" ")[3].replace("\"","") # delete " from satellites name
-                            # group by satellites
-                            if sat not in SATS_dict:
-                                SATS_dict[sat] = []
-                                the_line = [things.replace("\"", "") for things in line.split(" ")]
-                                SATS_dict[sat].append({'start_time': the_line[0],
-                                                       'stop_time': the_line[1],
-                                                       'DOI': the_line[2],
-                                                       'sat': the_line[3],
-                                                       'inst': the_line[4],
-                                                       'reg': the_line[5]})
-                            else:
-                                the_line = [things.replace("\"", "") for things in line.split(" ")]
-                                SATS_dict[sat].append({'start_time': the_line[0],
-                                                       'stop_time': the_line[1],
-                                                       'DOI': the_line[2],
-                                                       'sat': the_line[3],
-                                                       'inst': the_line[4],
-                                                       'reg': the_line[5]})
+for folders_or_pdf in os.listdir(main_path):
+    files_path = os.path.join(main_path, folders_or_pdf)
+    for files in os.listdir(files_path):
+        if files.endswith("bibheliotech_V1.txt"):
+            liste = []
+            with open(os.path.join(files_path,files)) as file:
+                Lines = file.readlines()
+                for line in Lines:
+                    if not re.search("^#",line): # skip comments lines
+                        sat = line.split(" ")[3].replace("\"","") # delete " from satellites name
+                        # group by satellites
+                        if sat not in SATS_dict:
+                            SATS_dict[sat] = []
+                            the_line = [things.replace("\"", "") for things in line.split(" ")]
+                            SATS_dict[sat].append({'start_time': the_line[0],
+                                                   'stop_time': the_line[1],
+                                                   'DOI': the_line[2],
+                                                   'sat': the_line[3],
+                                                   'inst': the_line[4],
+                                                   'reg': the_line[5]})
+                        else:
+                            the_line = [things.replace("\"", "") for things in line.split(" ")]
+                            SATS_dict[sat].append({'start_time': the_line[0],
+                                                   'stop_time': the_line[1],
+                                                   'DOI': the_line[2],
+                                                   'sat': the_line[3],
+                                                   'inst': the_line[4],
+                                                   'reg': the_line[5]})
 
 for elems,val in SATS_dict.items():
     SATS_dict[elems] = sorted(val, key=lambda d: d['start_time']) # sort by ascending start_time
